bc26c7c6beb62e93b6e5c3a3da66ceed69a3b879
TarekNexus <muhammadtarek5920@gmail.com>
2026-02-02 18:31:39 +0600

    middleware updated

--- src/modules/medicine/medicine.router.ts
+++ src/modules/medicine/medicine.router.ts
@@ -4,11 +4,10 @@ import { MedicineController } from "./medicine.controller";
 
 const router = Router();
 
-
-router.get("/", MedicineController.getAllMedicines);
+router.get("/categories/all", MedicineController.getAllCategories);
 router.get("/getMedicinesByCategory/:id", MedicineController.getMedicinesByCategory);
 router.get("/:id", MedicineController.getMedicineById);
-router.get("/categories/all", MedicineController.getAllCategories);
+router.get("/", MedicineController.getAllMedicines);
 
 
 
